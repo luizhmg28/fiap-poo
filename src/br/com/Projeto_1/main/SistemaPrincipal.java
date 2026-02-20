1605c425d73a4156347f5c7beda81e9fed0b47c8
Luiz Henrique Macedo Graça <luizhmg28@gmail.com>
2026-02-20 08:41:27 -0300

    feat: aula 01 - implementa classes do Desafio Pessoal

---
+++ src/br/com/Projeto_1/main/SistemaPrincipal.java
@@ -0,0 +1,17 @@
+package br.com.Projeto_1.main;
+
+import br.com.Projeto_1.model.CuboMagico;
+
+public class SistemaPrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CuboMagico tres_X_tres = new CuboMagico();
+		
+		tres_X_tres.quantidadeLados = 6;
+		tres_X_tres.preco = 50.0;
+		tres_X_tres.cores = "Amarelo, Branco, Verde, Azul, Vermelho, Laranja";
+	}
+
+}
---
+++ src/br/com/Projeto_1/model/CuboMagico.java
@@ -0,0 +1,8 @@
+package br.com.Projeto_1.model;
+
+public class CuboMagico {
+	public int quantidadeLados;
+	public double preco;
+	public String cores;
+	
+}
