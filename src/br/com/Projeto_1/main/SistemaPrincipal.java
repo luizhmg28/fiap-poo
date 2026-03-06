201db858e74e6183391745e39e44e9a0416bf340
Luiz Henrique <luizmhg28@gmail.com>
2026-03-06 09:19:59 -0300

    Inserção do projeto FiapRide

--- src/br/com/Projeto_1/main/SistemaPrincipal.java
+++ src/br/com/Projeto_1/main/SistemaPrincipal.java
@@ -1,17 +1,37 @@
 package br.com.Projeto_1.main;
 
-import br.com.Projeto_1.model.CuboMagico;
+import br.com.Projeto_1.model.Passageiro;
 
 public class SistemaPrincipal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		CuboMagico tres_X_tres = new CuboMagico();
-		
-		tres_X_tres.quantidadeLados = 6;
-		tres_X_tres.preco = 50.0;
-		tres_X_tres.cores = "Amarelo, Branco, Verde, Azul, Vermelho, Laranja";
-	}
+    public static void main(String[] args) {
+        
+        System.out.println("--- Iniciando o Sistema FiapRide --- \n");
+        
+        // INSTANCIAÇÃO (Usando o Construtor!)
+        // Não precisamos mais de passageiro.nome = "Ana", já passamos tudo de uma vez.
+        Passageiro passageiro1 = new Passageiro("Ana Silva", "222.222.222-22");
+        System.out.println(">>> Recarga Passageiro 1:");
+        passageiro1.adicionarSaldo(50.0);
+        
+        //Mesmo processo com o passageiro 2
+        Passageiro passageiro2 = new Passageiro("Carlos Souza", "333.333.333-33");
+        System.out.println(">>> Recarga Passageiro 2:");
+        passageiro2.adicionarSaldo(12.5);
 
+        System.out.println("\n--- Status dos Passageiros ---");
+        // Lendo os dados com os Getters:
+        System.out.println("Passageiro: " + passageiro1.getNome() + " | Saldo: R$ " + passageiro1.getSaldo() + " | CPF: " + passageiro1.getCpf());
+        System.out.println("Passageiro: " + passageiro2.getNome() + " | Saldo: R$ " + passageiro2.getSaldo() + " | CPF: " + passageiro2.getCpf());
+        
+        System.out.println("\n--- Realizando Viagens ---");
+        System.out.println("Pagando viagem do passageiro 1 (Ana)...");
+        passageiro1.pagarViagem(20.0); // Ana tem 50, vai sobrar 30.
+        
+        System.out.println("\nPagando viagem do passageiro 2 (Carlos)...");
+        passageiro2.pagarViagem(20.0); // Carlos tem 12.5. O sistema DEVE bloquear!
+        
+        // Tente hackear o sistema descomentando a linha abaixo:
+        //passageiro1.saldo = 999999.0; // O Java vai exibir erro vermelho! A proteção funcionou!
+    }
 }
